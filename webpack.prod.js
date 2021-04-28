e269f5824ff47bce49e2fed460b00fa09c8af19b
barzin <barzin144@yahoo.com>
2021-04-28 12:17:31 +0430

    add favicon to production

--- webpack.prod.js
+++ webpack.prod.js
@@ -16,6 +16,7 @@ module.exports = merge(common, {
     optimization: {
         minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin(), new HtmlWebpackPlugin({
             template: './src/index.html',
+            favicon: './src/images/favicon-32x32.png',
             minify: {
                 removeAttributeQuotes: true,
                 collapseWhitespace: true,
